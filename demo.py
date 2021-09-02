edc00abe768458ee2add7f96d7b9902a27ae71a2
HaeckelK <haeckelk.github@gmail.com>
2021-09-02 22:36:09 +0100

    feat: demo showing in-memory GET and POST for decks, notes and cards

---
+++ demo.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+
+from app import DeckOut, NoteOut
+
+
+def main():
+    base_url = "http://localhost:5000/"
+
+    # Create a deck
+    print("Create Deck")
+    response = requests.post(base_url + "decks", data={"name": "French"})
+    deck = DeckOut(**json.loads(response.content))
+    print(deck)
+
+    # Create a new note
+    print("Create Note")
+    response = requests.post(
+        base_url + "notes",
+        data={"text_front": "Hello", "text_back": "Bonjour", "deck_id": deck.id},
+    )
+    note = NoteOut(**json.loads(response.content))
+    print(note)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
