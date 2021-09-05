f531d61897c82c5bd76efda7c20f7c9cace2e737
HaeckelK <haeckelk.github@gmail.com>
2021-09-05 18:36:28 +0100

    feat: scheduler compares review window to card last review + interval

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from utils import timestamp
+from utils import timestamp, yesterday_midnight
 
 from models import ReviewOut
 
@@ -10,6 +10,13 @@ class Scheduler:
         self.new_cards_limit = new_cards_limit
         self.allow_cards_from_same_note = allow_cards_from_same_note
         self.total_cards_limit = total_cards_limit
+        self.review_time = -1
+        self.set_review_time()
+        return
+
+    def set_review_time(self) -> None:
+        """Wrapper function to ease test mocking."""
+        self.review_time = yesterday_midnight()
         return
 
     def create_reviews(self, reviews, cards) -> List[ReviewOut]:
@@ -23,6 +30,10 @@ class Scheduler:
         new_count = 0
         note_ids = set()
         for card in cards:
+            # Don't allow cards that are not due
+            if (card.time_latest_review + card.current_review_interval) > self.review_time:
+                continue
+
             # Don't allow cards from the same note to be reviewed on same day
             if (card.note_id in note_ids) and (self.allow_cards_from_same_note is False):
                 continue
