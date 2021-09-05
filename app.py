974b805d335cbacd8da69fe29a29dcc294cc1994
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-09-05 13:17:17 +0100

    Deck stats (#4)
    * feat: return cards_total with deck
    
    * feat: calculate card stats when adding new card
    
    * feat: get count_notes
    
    * feat: change card status
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-from typing import List
+from typing import List, Dict
 import time
 
 from flask import Flask, jsonify, request
@@ -15,9 +15,11 @@ class DeckIn:
 class DeckOut:
     id: int
     name: str
+    notes_total: int
     cards_total: int
     time_created: int
     count_reviews_due: int
+    count_new_cards: int
 
 
 @dataclass
@@ -89,6 +91,11 @@ CORS(app)
 
 @app.route("/decks", methods=["GET"])
 def get_decks():
+    # TODO should work this out after reviews?
+    # Update deck stats
+    card_count = get_count_cards_by_deck(status="new")
+    for deck in DECKS:
+        deck.count_new_cards = card_count.get(deck.id, 0)
     return jsonify([asdict(x) for x in DECKS])
 
 
@@ -166,6 +173,7 @@ def mark_review_correct(id: int):
             review.correct = True
             review.time_completed = timestamp()
             review.review_status = "reviewed"
+            change_card_status(review.card.id, status="seen")
     return "done"
 
 
@@ -176,6 +184,7 @@ def mark_review_incorrect(id: int):
             review.correct = False
             review.time_completed = timestamp()
             review.review_status = "reviewed"
+            change_card_status(review.card.id, status="seen")
     return "done"
 
 
@@ -205,7 +214,7 @@ def wipe_data():
 # Actual CRUD
 def add_new_deck(new_deck: DeckIn) -> DeckOut:
     id = len(DECKS) + 1
-    deck = DeckOut(id=id, name=new_deck.name, cards_total=0, time_created=timestamp(), count_reviews_due=123)
+    deck = DeckOut(id=id, name=new_deck.name, notes_total=0, cards_total=0, time_created=timestamp(), count_reviews_due=0, count_new_cards=0)
     DECKS.append(deck)
     return deck
 
@@ -214,6 +223,10 @@ def add_new_note(new_note: NoteIn) -> NoteOut:
     id = len(NOTES) + 1
     note = NoteOut(**asdict(new_note), id=id, time_created=timestamp())
     NOTES.append(note)
+    # Update deck stats
+    card_count = get_count_notes_by_deck()
+    for deck in DECKS:
+        deck.notes_total = card_count.get(deck.id, 0)
     return note
 
 
@@ -221,7 +234,44 @@ def add_new_card(new_card: CardIn):
     id = len(CARDS) + 1
     card = CardOut(id=id, **asdict(new_card), status="new", time_created=timestamp())
     CARDS.append(card)
+    # Update deck stats
+    card_count = get_count_cards_by_deck()
+    for deck in DECKS:
+        deck.cards_total = card_count.get(deck.id, 0)
     return
+
+
+def change_card_status(id: int, status: str):
+    for card in CARDS:
+        if int(card.id) == int(id):
+            card.status = status
+            return
+    return
+
+
+# Under the hood data store work
+def get_count_cards_by_deck(status="all") -> Dict[int, int]:
+    count = {}
+    if status == "all":
+        cards = [x for x in CARDS]
+    else:
+        cards = [x for x in CARDS if x.status == status]
+    for card in cards:
+        try:
+            count[card.deck_id] += 1
+        except  KeyError:
+            count[card.deck_id] = 1
+    return count
+
+
+def get_count_notes_by_deck() -> Dict[int, int]:
+    count = {}
+    for card in NOTES:
+        try:
+            count[card.deck_id] += 1
+        except  KeyError:
+            count[card.deck_id] = 1
+    return count
 
 
 # Utils
